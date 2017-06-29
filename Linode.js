b13751ba2bc0bb5d1631490542347e5b6347276b
Robin Jacobs <robin@robinj.be>
2017-06-29 17:21:33 +0100

    Initial commit

---
+++ Linode.js
@@ -0,0 +1,139 @@
+let Request = require ('request-promise');
+
+module.exports = class Linode
+{
+	constructor (apiKey, internal)
+	{
+		if (! internal)
+			return new Proxy (new Linode (apiKey, true), handler);
+		else
+			this.apiKey = apiKey;
+	}
+	
+	request (type, endpoint, data)
+	{
+		let uri = 'https://api.linode.com/v4/' + endpoint;
+		//if (type === 'GET')
+		//	uri += '?' + Object.keys (data).map ((v) => v + '=' + data[v]).join ('&');
+			
+		let options = {
+			uri: uri,
+			body: data,
+			headers: {
+				'Authorization': 'token ' + this.apiKey
+			},
+			json: true,
+			method: type
+		};
+		console.log (options);
+		return Request (options);
+	}
+}
+
+let handler = {
+	get: function (target, name)
+	{
+		return function (...params)
+		{
+			let method, resource, endpoint;
+			let data = {};
+			let methodMap = {
+				'create': 'POST',
+				'set': 'POST',
+				'edit': 'PUT',
+				'get': 'GET',
+				'list': 'GET',
+				'remove': 'DELETE'
+			};
+			let paramI1Resources = ['domains', 'nodebalancers', 'regions'];
+			
+			for (let prefix in methodMap)
+			{
+				if (name.toLowerCase ().startsWith (prefix))
+				{
+					method = methodMap[prefix];
+					resource = name.substr (prefix.length);
+				}
+			}
+			
+			if (! method)
+				throw new Error ('Invalid method');
+			
+			let ucIndexes = upperCaseIndexes (resource);
+			let endpointParts = [];
+			let start = 0;
+			let end = resource.length;
+			for (let i = start + 1; i < ucIndexes.length + 1; i++)
+			{
+				let index = i == ucIndexes.length - ucIndexes[i] ? end : ucIndexes[i];
+				endpointParts.push (pluralify (resource.toLowerCase ().substring (start, index)));
+				start = index;
+			}
+			
+			if (typeof params[params.length - 1] == 'object')
+				data = params.pop ();
+			
+			let paramFirstIndex = paramI1Resources.includes (endpointParts[0]) ? 1 : 2;
+			let paramIndexes = [];
+			for (let i = 0; i < params.length; i++)
+				paramIndexes.push (paramFirstIndex + i * 2);
+			let endpointPartCount = endpointParts.length;
+			while (paramIndexes.length > 0)
+				endpointParts.splice (paramIndexes.shift (), 0, params.shift ());
+			
+			return target.request (method, endpointParts.join ('/'), data);
+		}
+	}
+}
+
+function upperCaseIndexes (str)
+{
+	let indexes = [];
+	
+	for (let i = 0; i < str.length; i++)
+	{
+		if (str.charAt (i) !== str.charAt (i).toLowerCase ())
+			indexes.push (i);
+	}
+	
+	return indexes;
+}
+
+function pluralify (str)
+{
+	let map = {
+		'distribution': 'distributions',
+		'kernel': 'kernels',
+		'instance': 'instances',
+		'disk': 'disks',
+		'backup': 'backups',
+		'ip': 'ips',
+		'stackscript': 'stackscripts',
+		'type': 'types',
+		'domain': 'domains',
+		'nodebalancer': 'nodebalancers',
+		'balancer': 'nodebalancers',
+		'config': 'configs',
+		'node': 'nodes',
+		'network': 'networks',
+		'ipassign': 'ip-assign',
+		'region': 'regions',
+		'ticket': 'tickets',
+		'reply': 'replies',
+		'attachment': 'attachments',
+		'tfaenable': 'tfa-enable',
+		'tfaenableconfirm': 'tfa-enable-confirm',
+		'tfadisable': 'tfa-disable',
+		'grant': 'grants',
+		'token': 'tokens',
+		'setting': 'settings',
+		'client': 'clients',
+		'user': 'users',
+		'event': 'events'
+	};
+	
+	if (Object.keys (map).includes (str))
+		return map[str];
+	
+	return str;
+}
---
+++ README.md
@@ -0,0 +1,55 @@
+# Linode API
+
+Node.js Linode API based on their (currently experimental) v4 API.
+
+Methods are dynamic based on API endpoints specified in the documentation over here; https://developers.linode.com/v4
+
+## Usage
+
+Since methods calls on this API automatically generate the API URL to call you will need to have a look at Linode API v4's documentation.
+Methods are always in the format `actionResource`. So to create a domain, you would call `createDomain (data)` with `data` being an object containing the fields and their values as per API documentation. To get a Linode instance (based on the `/linode/instances/:id` API route) you would call `getLinodeInstance (id)`.
+
+URL parameters can be passed in in the order that they would appear in the route according to the documentation, and if there is a data object it can be passed along as the last parameter.
+
+Every call returns a Promise with the result returned by their API.
+
+### Actions
+
+`create` or `set` for `POST` requests, `edit` for `PUT` requests, `get` or `list` for `GET` requests, `remove` for `DELETE` requests.
+
+## Examples
+
+### Create domain
+
+API route: `/domains` (POST)
+
+```
+let lnc = new Linode ('api-key');
+lnc.createDomain ({ domain: 'example.org', type: 'master', soa_email: 'info@example.org' })
+	.then
+	(
+		(response) => {
+			console.log ('Domain ID: ' + response.id);
+		}
+	);
+```
+
+### Get all distributions
+
+API route: `/linode/distributions` (GET)
+
+```
+let lnc = new Linode ('api-key');
+lnc.getLinodeDistributions ()
+	.then (...);
+```
+
+### Get a Linode's disks
+
+API route: `/linode/instances/:id/disks`
+
+```
+let lnc = new Linode ('api-key');
+lnc.getLinodeInstanceDisks (linodeInstanceId)
+	.then (...);
+```
